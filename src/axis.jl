a869f08ca1c25b7a0105a47ffc8cf077bcb533ec
Tamás K. Papp <tkpapp@gmail.com>
2023-06-27 11:32:43 +0200

    cleanup of axis code

--- src/axis.jl
+++ src/axis.jl
@@ -1,39 +1,112 @@
+"""
+FIXME document
+
+# The user interface
+
+[`Interval`](@ref), [`bounds`](@ref), [`Linear`](@ref)
+
+# Within-package API
+
+FIXME document
+
+"""
 module Axis
 
 using ArgCheck: @argcheck
+using DocStringExtensions: SIGNATURES
 using ..PGF: Rectangle, Point
+
+####
+#### Interval arithmetic
+####
 
 struct Interval
     min::Float64
     max::Float64
+    @doc """
+    $(SIGNATURES)
+
+    A representation of the numbers `[min, max]`. It is required that `min ≤ max`, but `min
+    == max` is allowed.
+    """
     function Interval(min, max)
         @argcheck isfinite(min) && isfinite(max)
         @argcheck min ≤ max
         new(Float64(min), Float64(max))
     end
+    @doc """
+    $(SIGNATURES)
+
+    Represent the empty set ``∅``. Test with [`is_nonempty`](@ref).
+    """
     Interval() = new(Inf, -Inf) # sentinel for empty interval
 end
 
+"""
+$(SIGNATURES)
+
+Test whether the interval is the empty set.
+"""
 is_nonempty(a::Interval) = a.min ≤ a.max
 
-function extend(a::Interval, b::Real)
-    @argcheck isfinite(b)
-    Interval(min(a.min, b), max(a.max, b))
-end
+"""
+$(SIGNATURES)
 
-function extend(a::Interval, b::Interval)
-    @argcheck is_nonempty(b)
+The convex hull, ie the narrowest interval that contains both intervals.
+"""
+function hull(a::Interval, b::Interval)
     Interval(min(a.min, b.min), max(a.max, b.max))
 end
 
-function extend_xy((ax, ay)::Tuple{Interval,Interval},
-                 (bx, by)::Tuple{Interval,Interval})
-    (extend(ax, bx), extend(ay, by))
+"""
+$(SIGNATURES)
+
+Convex hull for a 2-tuple of intervals.
+"""
+function hull_xy((ax, ay)::T, (bx, by)::T) where {T<:Tuple{Interval,Interval}}
+    (hull(ax, bx), hull(ay, by))
 end
 
-bounds(f, itr) = Interval(extrema(f, itr)...)
+"""
+$(SIGNATURES)
 
-bounds_xy(itr) = mapreduce(bounds_xy, extend_xy, itr; init = (Interval(), Interval()))
+A helper function that wraps `extrema(f, itr)` in an `Interval`.
+"""
+bounds(f, itr) = isempty(itr) ? Interval() : Interval(extrema(f, itr)...)
+
+bounds_xy(itr) = mapreduce(bounds_xy, hull_xy, itr; init = (Interval(), Interval()))
+
+####
+#### Axis
+####
+
+###
+### Generic axis API
+###
+
+function finalize end
+
+function coordinate_to_unit end
+
+Base.@kwdef struct DrawingArea{TX,TY}
+    finalized_x_axis::TX
+    finalized_y_axis::TY
+    rectangle::Rectangle
+end
+
+function coordinates_to_point(drawing_area::DrawingArea, (x, y))
+    @argcheck isfinite(x) && isfinite(y) "Non-finite coordinates."
+    (; finalized_x_axis, finalized_y_axis, rectangle) = drawing_area
+    x_u = coordinate_to_unit(finalized_x_axis, x)
+    y_u = coordinate_to_unit(finalized_y_axis, y)
+    _f(z, min, max) = z * (max - min) + min
+    Point(_f(x_u, rectangle.left, rectangle.right),
+          _f(y_u, rectangle.bottom, rectangle.top))
+end
+
+###
+### Linear axis
+###
 
 struct Linear end
 
@@ -52,22 +125,6 @@ end
 function coordinate_to_unit(finalized_axis::LinearFinalized, x::Real)
     (; min, max) = finalized_axis.interval
     (x - min) / (max - min)
-end
-
-Base.@kwdef struct DrawingArea{TX,TY}
-    finalized_x_axis::TX
-    finalized_y_axis::TY
-    rectangle::Rectangle
-end
-
-function coordinates_to_point(drawing_area::DrawingArea, (x, y))
-    @argcheck isfinite(x) && isfinite(y) "Non-finite coordinates."
-    (; finalized_x_axis, finalized_y_axis, rectangle) = drawing_area
-    x_u = coordinate_to_unit(finalized_x_axis, x)
-    y_u = coordinate_to_unit(finalized_y_axis, y)
-    _f(z, min, max) = z * (max - min) + min
-    Point(_f(x_u, rectangle.left, rectangle.right),
-          _f(y_u, rectangle.bottom, rectangle.top))
 end
 
 end
