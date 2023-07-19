6ad52754b2b4a918a686c7498417deb0964c2888
Tamás K. Papp <tkpapp@gmail.com>
2023-07-19 12:31:49 +0200

    axis ticks, transforms cleanup & docs

--- src/axis.jl
+++ src/axis.jl
@@ -13,7 +13,7 @@ FIXME document
 module Axis
 
 using ArgCheck: @argcheck
-using DocStringExtensions: SIGNATURES
+using DocStringExtensions: FUNCTIONNAME, SIGNATURES
 import ..PGF
 using ..Intervals
 using ..Ticks
@@ -32,6 +32,13 @@ bounds_xy(itr) = mapreduce(bounds_xy, hull_xy, itr)
 
 function finalize end
 
+"""
+$(FUNCTIONNAME)(finalized_axis, x)
+
+Map the coordinate to `[0, 1]` using `finalized_axis`.
+
+When the rectangle is known during rendering, use [`unit_to_canvas`](@ref).
+"""
 function coordinate_to_unit end
 
 Base.@kwdef struct DrawingArea{TX,TY}
@@ -40,14 +47,20 @@ Base.@kwdef struct DrawingArea{TX,TY}
     rectangle::PGF.Rectangle
 end
 
+"""
+$(SIGNATURES)
+
+Transform a coordinate in `[0,1]` to `[a,b]`.
+"""
+unit_to_canvas(a, b, z) = a + (b - a) * z
+
 function coordinates_to_point(drawing_area::DrawingArea, (x, y))
     @argcheck isfinite(x) && isfinite(y) "Non-finite coordinates."
     (; finalized_x_axis, finalized_y_axis, rectangle) = drawing_area
     x_u = coordinate_to_unit(finalized_x_axis, x)
     y_u = coordinate_to_unit(finalized_y_axis, y)
-    _f(z, min, max) = z * (max - min) + min
-    PGF.Point(_f(x_u, rectangle.left, rectangle.right),
-              _f(y_u, rectangle.bottom, rectangle.top))
+    PGF.Point(unit_to_canvas(rectangle.left, rectangle.right, x_u),
+              unit_to_canvas(rectangle.bottom, rectangle.top, y_u))
 end
 
 ###
@@ -92,15 +105,24 @@ function PGF.render(io::IO, rectangle::PGF.Rectangle, axis::FinalizedLinear; ori
     is_x = orientation == :x
     !is_x && @argcheck orientation == :y "orientation has to be :x or :y"
     (; interval, ticks, style) = axis
-    gap(l) = is_x ? PGF.Point(0, l) : PGF.Point(l, 0)
-    (; line_gap, line_width) = style
-    a = is_x ? PGF.Point(rectangle.left, rectangle.top) : PGF.Point(rectangle.right, rectangle.bottom)
-    b = is_x ? PGF.Point(rectangle.right, rectangle.top) : PGF.Point(rectangle.right, rectangle.top)
+    _point(x, y) = is_x ? PGF.Point(x, y) : PGF.Point(y, x)
+    (; line_gap, line_width, tick_length) = style
+    a = is_x ? rectangle.left :  rectangle.bottom
+    b = is_x ? rectangle.right : rectangle.top
+    edge = is_x ? rectangle.top : rectangle.right
+    y1 = edge - line_gap        # line, tick start
+    y2 = y1 - tick_length       # tick end
     PGF.setstrokecolor(io, PGF.BLACK)
     PGF.setlinewidth(io, line_width)
-    PGF.pathmoveto(io, a - gap(line_gap))
-    PGF.pathlineto(io, b - gap(line_gap))
+    PGF.pathmoveto(io, _point(a, y1))
+    PGF.pathlineto(io, _point(b, y1))
     PGF.usepathqstroke(io)
+    for (pos, label) in ticks
+        x = unit_to_canvas(a, b, coordinate_to_unit(axis, pos))
+        PGF.pathmoveto(io, _point(x, y1))
+        PGF.pathlineto(io, _point(x, y2))
+        PGF.usepathqstroke(io)
+    end
 end
 
 end
