36deb0c9f99c0864ecd1fe47af673f96bb4025d0
Tamás K. Papp <tkpapp@gmail.com>
2024-02-19 14:39:14 +0100

    move plot code into separate files

---
+++ src/plots/elements.jl
@@ -0,0 +1,184 @@
+####
+#### common plot components to visualize data
+####
+
+export Lines, Scatter, Circles, RelativeBars
+
+####
+#### Lines
+####
+
+struct Lines
+    coordinates
+    line_width::LENGTH
+    color
+    dash::PGF.Dash
+    @doc """
+    $(SIGNATURES)
+    """
+    function Lines(coordinates; line_width = DEFAULTS.line_width,
+                   color = DEFAULTS.line_color, dash::PGF.Dash = LINE_SOLID)
+        new(ensure_vector(coordinates), _length_positive(line_width), color, dash)
+    end
+end
+
+bounds_xy(lines::Lines) = bounds_xy(lines.coordinates)
+
+function PGF.render(sink::PGF.Sink, drawing_area::DrawingArea, lines::Lines)
+    (; coordinates, line_width, color, dash) = lines
+    peeled = Iterators.peel(coordinates)
+    peeled ≡ nothing && return
+    set_line_style(sink; color, width = line_width, dash)
+    c1, cR = peeled
+    PGF.pathmoveto(sink, coordinates_to_point(drawing_area, c1))
+    for c in cR
+        PGF.pathlineto(sink, coordinates_to_point(drawing_area, c))
+    end
+    PGF.usepathqstroke(sink)
+end
+
+###
+### Scatter
+###
+
+struct Scatter{M}
+    coordinates::AbstractVector
+    mark::M
+    @doc """
+    $(SIGNATURES)
+
+    A scatterplot.
+
+    See [`MarkSymbol`](@ref) and [`MarkQ5`](@ref).
+    """
+    function Scatter(mark::M, coordinates) where {M}
+        new{M}(ensure_vector(coordinates), mark)
+    end
+end
+
+Scatter(coordinates) = Scatter(MarkSymbol(), coordinates)
+
+bounds_xy(scatter::Scatter) = bounds_xy(scatter.coordinates)
+
+function PGF.render(sink::PGF.Sink, drawing_area::DrawingArea, scatter::Scatter)
+    (; mark, coordinates) = scatter
+    for xy in coordinates
+        PGF.render(sink, drawing_area, mark, xy)
+    end
+end
+
+function print_tex(sink::PGF.Sink, plot::Plot; standalone::Bool = false)
+    print_tex(sink, Canvas(plot); standalone)
+end
+
+###
+### Circles
+###
+
+struct Circles
+    x_y_w::AbstractVector
+    scale::LENGTH
+    fill_color::Union{Nothing,COLOR}
+    stroke_color::Union{Nothing,COLOR}
+    stroke_width::LENGTH
+    @doc """
+    $(SIGNATURES)
+
+    Taking an iterator or vector of `(x, y, w)` triplets (eg `NTuple{3}`, but anything
+    iterable with 3 elements will do), draw circles centered on `(x, y)` coordinates
+    with radius `scale * √w`.
+
+    # Keyword arguments
+
+    `stroke_color` determines the stroke color, using `nothing` if circles should not be
+    stroked. `stroke_width` determines the stroke with if applicable.
+
+    `fill_color` determines the fill color of circles.
+    """
+    function Circles(x_y_w, scale;
+                     stroke_color = nothing,
+                     stroke_width = DEFAULTS.line_width,
+                     fill_color = DEFAULTS.fill_color)
+        new(collect(x_y_w),
+            _length_positive(scale),
+            convert_maybe(COLOR, fill_color),
+            convert_maybe(COLOR, stroke_color),
+            _length_positive(stroke_width))
+    end
+end
+
+bounds_xy(circles::Circles) = bounds_xy(circles.x_y_w)
+
+function PGF.render(sink::PGF.Sink, drawing_area::DrawingArea, circles::Circles)
+    (; x_y_w, scale, stroke_color, stroke_width, fill_color) = circles
+    set_stroke_or_fill_style(sink; stroke_color, fill_color, stroke_width)
+    for (x, y, w) in x_y_w
+        PGF.pathcircle(sink, coordinates_to_point(drawing_area, (x, y)), scale * √w)
+        path_q_stroke_or_fill(sink, stroke_color, fill_color)
+    end
+end
+
+####
+#### RelativeBars
+####
+
+struct RelativeBars
+    orientation::Symbol
+    edges_and_values::Vector{Tuple{Float64,Float64,Float64}}
+    baseline::Float64
+    fill_color::Union{Nothing,COLOR}
+    stroke_color::Union{Nothing,COLOR}
+    stroke_width::LENGTH
+    """
+    $(SIGNATURES)
+
+    Draw bars relative to `baseline` with the given `orientation`, which may be
+    `:horizontal` or `:vertical`.
+
+    The second argument should be an iterable of `(e1, e2, v)` values, all finite, which
+    specify the edges and the value for each bar (`baseline` provides the other value).
+
+    At least `fill_color` or `stroke_color` should be specified.
+
+    Note that for most applications, an algorithm would calculate the edges and values,
+    and such a method should be provided. For example, when `StatsBase` is loaded, a
+    `Histogram` is a valid second argument and will be converted accordingly.
+    """
+    function RelativeBars(orientation::Symbol, edges_and_values; baseline = 0,
+                          stroke_color = DEFAULTS.bars_stroke_color,
+                          stroke_width = DEFAULTS.line_width,
+                          fill_color = DEFAULTS.fill_color)
+        @argcheck orientation ∈ (:vertical, :horizontal)
+        @argcheck isfinite(baseline)
+        new(orientation,
+            map(x -> (_x = convert(Tuple{Float64,Float64,Float64}, x);
+                      @argcheck all(isfinite, _x);
+                      @argcheck _x[1] < _x[2];
+                      _x),
+                edges_and_values),
+            convert(Float64, baseline),
+            convert_maybe(COLOR, fill_color),
+            convert_maybe(COLOR, stroke_color),
+            convert(LENGTH, stroke_width))
+    end
+end
+
+function bounds_xy(relative_bars::RelativeBars)
+    (; orientation, edges_and_values, baseline) = relative_bars
+    e = mapreduce(x -> Interval(x[1], x[2]), hull, edges_and_values)
+    v = Interval(extrema((baseline, extrema(x -> x[3], edges_and_values)...))...)
+    orientation ≡ :vertical ? (e, v) : (v, e)
+end
+
+function PGF.render(sink::PGF.Sink, drawing_area::DrawingArea, relative_bars::RelativeBars)
+    (; orientation, edges_and_values, baseline, stroke_color, stroke_width,
+     fill_color) = relative_bars
+    set_stroke_or_fill_style(sink; stroke_color, fill_color, stroke_width)
+    for (e1, e2, v) in edges_and_values
+        c1 = coordinates_to_point(drawing_area, orientation ≡ :vertical ?
+            (e1, baseline) : (baseline, e1))
+        c2 = coordinates_to_point(drawing_area, orientation ≡ :vertical ? (e2, v) : (v, e2))
+        PGF.path(sink, PGF.Rectangle(c1, c2))
+        path_q_stroke_or_fill(sink, stroke_color, fill_color)
+    end
+end
---
+++ src/plots/sync_bounds.jl
@@ -0,0 +1,118 @@
+#####
+##### syncing plot boundaries
+#####
+
+export Invisible, sync_bounds
+
+####
+#### Invisible
+####
+
+struct Invisible
+    xy::Tuple{CoordinateBounds,CoordinateBounds}
+    @doc """
+    $(SIGNATURES)
+
+    Create an invisible object with the sole function of extending coordinate bounds to (x,
+    y), which should be `Interval`s or `∅`.
+
+    You can also use `Invisible(bounds_xy(object))` to extend bounds to those in `object`.
+    """
+    function Invisible(xy::Tuple{CoordinateBounds,CoordinateBounds})
+        new(xy)
+    end
+end
+
+bounds_xy(invisible::Invisible) = invisible.xy
+
+PGF.render(sink::PGF.Sink, drawing_area::DrawingArea, ::Invisible) = nothing
+
+####
+#### sync_bounds
+####
+
+"""
+$(SIGNATURES)
+
+Add an `Invisible(xy)` to each plot in `itr`. Internal helper function.
+"""
+function _add_invisible(xy::Tuple{CoordinateBounds,CoordinateBounds}, itr)
+    invisible = Invisible(xy)
+    map(x -> @insert(last(x.contents) = invisible), itr)
+end
+
+"""
+$(SIGNATURES)
+
+Make sure that axis bounds are the same for axes as determined
+by `tag` (see below).
+
+Tag can be given in the form of `Val(tag)` too, this is a convenience wrapper.
+
+Possible tags:
+
+- `:X`, `:Y`, `:XY`: make the specified axes in *all* items of the collection the same
+
+- `:x`, `:y`, `:xy`: make x/y axes the same in plots that are in the same column/row, only works for
+
+`:x`, `:y`, and `:xy` only work for matrix-like arguments.
+
+All methods return the (modified) first argument.
+
+# Explanation
+
+To illustrate the lowercase tags, consider the arrangement
+
+```ascii
+y/vertical axis
+│
+│ C  D
+│ A  B
+└────────x/horizontal axis
+```
+which could be entered as eg
+```julia
+t = Tableau([A C; B D])
+```
+Then `$(FUNCTIONNAME)(:x, t)` would ensure that A and C have the same bounds for the
+x-axis, and similarly B and D.
+"""
+@inline function sync_bounds(tag::Symbol, collection)
+    @argcheck tag ∈ (:x, :y, :xy, :X, :Y, :XY)
+    sync_bounds(Val(tag), collection)
+end
+
+sync_bounds(tag::Val) = Base.Fix1(sync_bounds, tag)
+
+@inline sync_bounds(tag::Symbol) = sync_bounds(Val(tag))
+
+function sync_bounds(tag::Val{:X}, collection)
+    # vectors are treated like 1×N matrices, x axes are synced
+    xb, _ = bounds_xy(collection)
+    _add_invisible((xb, ∅), collection)
+end
+
+function sync_bounds(tag::Val{:Y}, collection)
+    _, yb = bounds_xy(collection)
+    _add_invisible((∅, yb), collection)
+end
+
+sync_bounds(tag::Val{:XY}, collection) = _add_invisible(bounds_xy(collection), collection)
+
+function sync_bounds(tag::Union{Val{:x},Val{:y},Val{:xy}}, collection::T) where T
+    if Base.IteratorSize(T) == Base.HasShape{2}()
+        sync_bounds(tag, collect(collection))
+    else
+        throw(ArgumentError("Tag $(tag) only works for matrix-like arguments."))
+    end
+end
+
+function sync_bounds(::Val{:x}, m::AbstractMatrix)
+    mapreduce(row -> permutedims(sync_bounds(Val(:X), row)), vcat, eachrow(m))
+end
+
+function sync_bounds(::Val{:y}, m::AbstractMatrix)
+    mapreduce(col -> sync_bounds(Val(:Y), col), hcat, eachcol(m))
+end
+
+sync_bounds(::Val{:xy}, m::AbstractMatrix) = sync_bounds(Val(:x), sync_bounds(Val(:y), m))
